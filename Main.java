fab0c761b7810f8807fbdbccf4977bf9f79f64a7
Enrique <proenrique4@gmail.com>
2025-09-09 09:04:07 -0700

    prueba 1

--- Main.java
+++ Main.java
@@ -1,5 +1,33 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 public class Main {
-    
+    public static void main(String[] args)throws Exception {
+
+        Socket salida = new Socket("Localhost",8080); 
+
+
+       PrintWriter escritor = new PrintWriter(salida.getOutputStream(),true);
+       BufferedReader lector = new BufferedReader(new InputStreamReader(salida.getInputStream()));
+       BufferedReader teclado = new BufferedReader(new InputStreamReader(System.in));
+        String mensaje;
+       String cadena = teclado.readLine();
+       
+    while (!cadena.equalsIgnoreCase("FIN")){
+           escritor.println(cadena);
+           mensaje = lector.readLine();
+           System.out.println(mensaje);
+           cadena= teclado.readLine();  
+           
+       }
+      salida.close();
+    }
+}
 
     
-}
+
+
+    
+
